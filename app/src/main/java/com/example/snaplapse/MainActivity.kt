192ab290a41f273744d9808e178809ac4c584cfc
RAYMOND XIA <USERNAME@uwaterloo.ca>
2022-07-21 06:53:50 +0100

    init timeline page

--- app/src/main/java/com/example/snaplapse/MainActivity.kt
+++ app/src/main/java/com/example/snaplapse/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.snaplapse
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val switchToSecondActivity:Button = findViewById(R.id.timelineButton)
+        switchToSecondActivity.setOnClickListener {
+            val intent = Intent(this@MainActivity, TimelineActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
