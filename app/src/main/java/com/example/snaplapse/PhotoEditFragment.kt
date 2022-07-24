c4c2262cc19e8821900b2ed79735e1fd8c7e900a
kcchik <kevin.c.chik@gmail.com>
2022-07-24 19:26:20 -0400

    proper back button functionality

--- app/src/main/java/com/example/snaplapse/PhotoEditFragment.kt
+++ app/src/main/java/com/example/snaplapse/PhotoEditFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.activityViewModels
 import com.example.snaplapse.databinding.FragmentPhotoEditBinding
 
@@ -26,6 +27,14 @@ class PhotoEditFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        val callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                val transaction = parentFragmentManager.beginTransaction()
+                transaction.add(R.id.fragmentContainerView, CameraFragment())
+                transaction.commit()
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, callback)
         _binding = FragmentPhotoEditBinding.inflate(inflater, container, false)
         return binding.root
     }
