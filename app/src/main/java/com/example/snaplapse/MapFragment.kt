f94e227698cc030b1e20e3186476415d9bd81e3f
Agamjyot Sidhu <agam@Agamjyots-MBP.home>
2022-07-25 02:41:08 -0400

    added zoom to marker on DP library

--- app/src/main/java/com/example/snaplapse/MapFragment.kt
+++ app/src/main/java/com/example/snaplapse/MapFragment.kt
@@ -1,24 +1,30 @@
 package com.example.snaplapse
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import android.location.Location
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.common.api.Api
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 
-class MapFragment : Fragment(), OnMapReadyCallback {
+class MapFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
     private lateinit var mMap: GoogleMap
     //private lateinit var client: FusedLocationProviderClient
-    //private lateinit var activity: MainActivity
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,23 +35,32 @@ class MapFragment : Fragment(), OnMapReadyCallback {
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
-        //client = LocationServices.getFusedLocationProviderClient(activity)
+        //client = LocationServices.getFusedLocationProviderClient(mainAct)
         return rootView
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
         mMap.uiSettings.isZoomControlsEnabled = true
+        mMap.setOnMarkerClickListener(this)
 
         setUpMap()
     }
 
+    @SuppressLint("MissingPermission")
     private fun setUpMap() {
-        //heres where location code would go
-        mMap.addMarker(
-            MarkerOptions()
-                .position(LatLng(0.0, 0.0))
-                .title("Marker")
-        )
+        val currentLatLong = LatLng(43.469921938973776, -80.54219761274572)
+        placeMarkerOnMap(currentLatLong)
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLong, 12f))
+
     }
+
+    private fun placeMarkerOnMap(currentLatLong: LatLng) {
+        val markerOptions = MarkerOptions().position(currentLatLong)
+        markerOptions.title("$currentLatLong")
+        mMap.addMarker(markerOptions)
+    }
+
+    //could add in event here to expand posts
+    override fun onMarkerClick(p0: Marker) = false
 }
