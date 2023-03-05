bfe9db844d5e9ac0300a2eb2e0b2a8a3b728ac3d
Daniel Kim <daniel.k.0710@gmail.com>
2023-03-04 22:02:51 -0500

    implemented find place api

--- app/src/main/java/com/example/snaplapse/api/routes/MapsApi.kt
+++ app/src/main/java/com/example/snaplapse/api/routes/MapsApi.kt
@@ -7,4 +7,7 @@ import retrofit2.http.QueryMap
 interface MapsApi {
     @GET("/maps/api/place/nearbysearch/json")
     suspend fun getNearbyPlaces(@QueryMap params: Map<String, String>): Response<String>
+
+    @GET("/maps/api/place/findplacefromtext/json")
+    suspend fun findPlaceFromText(@QueryMap params: Map<String, String>): Response<String>
 }
