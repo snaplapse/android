c75138a2d5595e9fb6e9cf854a1d4677fa2670b0
Daniel Kim <daniel.k.0710@gmail.com>
2023-03-06 01:17:53 -0500

    donezo

--- app/src/main/java/com/example/snaplapse/camera/LocationDialogFragment.kt
+++ app/src/main/java/com/example/snaplapse/camera/LocationDialogFragment.kt
@@ -8,12 +8,15 @@ import android.util.Log
 import androidx.core.os.bundleOf
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.setFragmentResult
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.snaplapse.MainActivity
 import com.example.snaplapse.R
 import com.example.snaplapse.view_models.CurrentPlaceViewModel
 
-class LocationDialogFragment: DialogFragment() {
+class LocationDialogFragment: DialogFragment(), LocationAdapter.RadioButtonCallback {
     var currentItem: Int = 0
-    var items = ArrayList<CurrentPlaceViewModel>()
+    var items = mutableListOf<CurrentPlaceViewModel>()
 
     fun setItem(index: Int) {
         currentItem = index
@@ -26,25 +29,30 @@ class LocationDialogFragment: DialogFragment() {
     }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        var arrayItems = arrayOf(items[0].name)
-        for (i in 1 until items.size) {
-            arrayItems += items[i].name
-        }
-
         return activity?.let {
             val builder = AlertDialog.Builder(context)
-            val originalItem = currentItem
-            builder.setTitle("Select Location")
-                .setSingleChoiceItems(arrayItems, currentItem, DialogInterface.OnClickListener { dialog, which ->
-                    currentItem = which
-                })
+            val inflater = requireActivity().layoutInflater
+            val view = inflater.inflate(R.layout.fragment_location_dialog, null)
+
+            val recyclerview = view.findViewById<RecyclerView>(R.id.location_recycler_view)
+            recyclerview.layoutManager = LinearLayoutManager(MainActivity())
+            val adapter = LocationAdapter(items, currentItem, this)
+            recyclerview.adapter = adapter
+
+            builder.setView(view)
+                .setTitle("Select Location")
+
                 .setPositiveButton("Confirm", DialogInterface.OnClickListener { dialog, id ->
                     setFragmentResult("locationDialog", bundleOf("locationKey" to currentItem))
                 })
                 .setNegativeButton("Cancel", DialogInterface.OnClickListener { dialog, id ->
-                    currentItem = originalItem
                 })
             builder.create()
         } ?: throw IllegalStateException("Activity cannot be null")
+    }
+
+    override fun changeLocation(location: Int) {
+        currentItem = location
+        Log.i("location", location.toString())
     }
 }
